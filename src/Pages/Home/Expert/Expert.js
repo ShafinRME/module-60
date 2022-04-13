93334a057c027bd568d16ec0e481805a592191a8
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-13 21:51:38 +0600

    Alhamdulillah.Carousel is done by using react-bootstrap

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -3,14 +3,17 @@ import React from 'react';
 const Expert = ({ expert }) => {
     const { name, img } = expert;
     return (
-        <div class="card col-sm-12 col-md-6 col-lg-4 gx-5" style={{ width: "18rem" }}>
-            <img src={img} class="card-img-top" alt="..." />
-            <div class="card-body">
-                <h5 class="card-title">{name}</h5>
-                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <div className='col-sm-12 col-md-6 col-lg-4 g-5 '>
+            <div class="card " style={{ width: "18rem" }}>
+                <img src={img} class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">{name}</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
             </div>
         </div>
+
     );
 };
 
