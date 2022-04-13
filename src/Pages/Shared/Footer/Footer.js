0f29803a44993b9cdfc49d7b4d21a0c3cc3409ad
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-13 22:04:11 +0600

    Alhamdulillah.Completed

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className='text-center m-5'>
             <p><small>Copyright &copy; {new Date().getFullYear()}</small></p>
         </footer>
     );
