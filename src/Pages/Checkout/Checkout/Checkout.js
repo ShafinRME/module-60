241e9229d54b8e31dfbed520b71d7960ce773669
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-28 17:15:56 +0600

    Order place is completed

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom';
 import useServiceDetail from '../../../hooks/useServiceDetail';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Checkout = () => {
     const { serviceId } = useParams();
@@ -31,15 +33,23 @@ const Checkout = () => {
             address: event.target.address.value,
             phone: event.target.phone.value
         }
+        axios.post('http://localhost:5000/order', order)
+            .then(response => {
+                const { data } = response;
+                if (data.insertedId) {
+                    toast('Your order is booked!!!');
+                    event.target.reset();
+                }
+            })
     }
 
     return (
         <div className='w-50 mx-auto'>
             <h2>Please Order: {service.name}</h2>
             <form onSubmit={handlePlaceOrder}>
-                <input className='w-100 mb-2' type="text" value={user.displayName} name="name" placeholder='Name' required readOnly disabled /> <br />
-                <input className='w-100 mb-2' type="email" value={user.email} name="email" placeholder='Email' required readOnly disabled /> <br />
-                <input className='w-100 mb-2' type="text" value={service.name} name="service" placeholder='Service' required /> <br />
+                <input className='w-100 mb-2' type="text" value={user?.displayName} name="name" placeholder='Name' required readOnly disabled /> <br />
+                <input className='w-100 mb-2' type="email" value={user?.email} name="email" placeholder='Email' required readOnly disabled /> <br />
+                <input className='w-100 mb-2' type="text" value={service.name} name="service" placeholder='Service' required readOnly /> <br />
                 <input className='w-100 mb-2' type="text" name="address" placeholder='Address' autoComplete='off' required /> <br />
                 <input className='w-100 mb-2' type="text" value={user.phone} name="phone" placeholder='Phone' required /> <br />
                 <input type="submit" value="Place Order" />
