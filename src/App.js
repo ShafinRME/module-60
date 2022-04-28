0f9a64e0e5fb43e1d18f3c130ec51365391620b3
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-28 15:52:57 +0600

    Alhamdulillah.Module 68 is starting

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <Route path="/about" element={<About></About>}></Route>
         <Route path="/login" element={<Login></Login>}></Route>
         <Route path="/register" element={<Register></Register>}></Route>
-        <Route path="/checkout" element={<RequireAuth><Checkout></Checkout></RequireAuth>}></Route>
+        <Route path="/checkout/:serviceId" element={<RequireAuth><Checkout></Checkout></RequireAuth>}></Route>
         <Route path="/addservice" element={<RequireAuth><AddService></AddService></RequireAuth>}></Route>
         <Route path="/manage" element={<RequireAuth><ManageServices></ManageServices></RequireAuth>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
