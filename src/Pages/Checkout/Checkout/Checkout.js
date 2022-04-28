5e6729fd5f26e9dc62a0f49c42142b08b69126d1
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-28 16:30:48 +0600

    Checkout from and auto fillup is done

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useServiceDetail from '../../../hooks/useServiceDetail';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Checkout = () => {
     const { serviceId } = useParams();
     const [service] = useServiceDetail(serviceId);
+    const [user] = useAuthState(auth);
+
+    // const [user, setUser] = useState({
+    //     name: 'Akbar the great',
+    //     email: 'akbarmomtax@gmail.com',
+    //     address: 'Tajmohol Road, M.pur',
+    //     phone: '0173777272772'
+    // })
+    // const handleAddressChange = event => {
+    //     const { address, ...rest } = user;
+    //     const newAddress = event.target.value;
+    //     const newUser = { address: newAddress, ...rest };
+    //     setUser(newUser);
+
+    // }
+    const handlePlaceOrder = event => {
+        event.preventDefault();
+        const order = {
+            email: user.email;
+            service: service.name,
+            serviceId: serviceId,
+            address: event.target.address.value;
+            phone: event.target.phone.value;
+        }
+    }
+
     return (
         <div className='w-50 mx-auto'>
             <h2>Please Order: {service.name}</h2>
-            <form>
-                <input type="text" name="name" placeholder='Name' required />
+            <form onSubmit={handlePlaceOrder}>
+                <input className='w-100 mb-2' type="text" value={user.displayName} name="name" placeholder='Name' required readOnly disabled /> <br />
+                <input className='w-100 mb-2' type="email" value={user.email} name="email" placeholder='Email' required readOnly disabled /> <br />
+                <input className='w-100 mb-2' type="text" value={service.name} name="service" placeholder='Service' required /> <br />
+                <input className='w-100 mb-2' type="text" name="address" placeholder='Address' autoComplete='off' required /> <br />
+                <input className='w-100 mb-2' type="text" value={user.phone} name="phone" placeholder='Phone' required /> <br />
+                <input type="submit" value="Place Order" />
             </form>
         </div>
     );
